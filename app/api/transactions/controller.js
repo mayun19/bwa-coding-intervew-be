a0ce61f786912319e8c898e0e3f743fdb7c7498a
mayun19 <mutiara.ayun19@gmail.com>
2023-11-16 23:31:14 +0700

    upload project

---
+++ app/api/transactions/controller.js
@@ -0,0 +1,55 @@
+const { Transaction, DetailTransaction } = require("../../db/models");
+const {Op} = require('sequelize')
+
+module.exports = {
+  getTransactionList: async (req, res, next) => {
+    try {
+      const { keyword = "" } = req.query;
+
+      let condition = {
+        user: req.user.id,
+      };
+
+      if (keyword !== "") {
+        condition = { ...condition, invoice: { [Op.like]: `%${keyword}%` } };
+      }
+
+      const transaction = await Transaction.findAll({
+        where: condition,
+        include: {
+          model: DetailTransaction,
+          as: "detailTransaction",
+        },
+      });
+
+      console.log(transaction);
+
+      res.status(200).json({
+        message: "Success get all trasaction",
+        data: transaction,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+  detailTransaction: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      const detailTransaction = await Transaction.findOne({
+        where: {id: id},
+        include: {
+          model: DetailTransaction,
+          as: "detailTransaction",
+        },
+      });
+
+      res.status(200).json({
+        message: "Success get all trasaction",
+        data: detailTransaction,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
